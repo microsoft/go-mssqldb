274d4506cad031ea106769227b4f9214e168d1e6
David Shiflet <david.shiflet@microsoft.com>
2022-10-27 15:09:34 -0400

    fix build

--- namedpipe/stub.go
+++ namedpipe/stub.go
@@ -1,4 +1,5 @@
 //go:build !windows
+
 package namedpipe
 
 func (n namedPipeDialer) ParseServer(server string, p *msdsn.Config) error {
@@ -14,8 +15,8 @@ func (n namedPipeDialer) ParseBrowserData(data msdsn.BrowserData, p *msdsn.Confi
 }
 
 func (n namedPipeDialer) DialConnection(ctx context.Context, p msdsn.Config) (conn net.Conn, err error) {
-	
-	return nil, return fmt.Errorf("Named pipe connections are not supported on this operating system")
+
+	return nil, fmt.Errorf("Named pipe connections are not supported on this operating system")
 }
 
 func (n namedPipeDialer) CallBrowser(p *msdsn.Config) bool {
