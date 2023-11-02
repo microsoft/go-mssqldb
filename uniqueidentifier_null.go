ccd7616aa7f43414a854fd0c59148f60a2b967cb
Norman Gehrsitz <git@gehrsitz.eu>
2023-11-02 23:48:27 +0100

    Add Valid bool to NullUniqueIdentifier

--- uniqueidentifier_null.go
+++ uniqueidentifier_null.go
@@ -4,34 +4,46 @@ import (
 	"database/sql/driver"
 )
 
-type NullUniqueIdentifier UniqueIdentifier
+type NullUniqueIdentifier struct {
+	UUID  UniqueIdentifier
+	Valid bool // Valid is true if UUID is not NULL
+}
 
 func (n *NullUniqueIdentifier) Scan(v interface{}) error {
-	u := UniqueIdentifier(*n)
 	if v == nil {
-		*n = [16]byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0}
+		*n = NullUniqueIdentifier{
+			UUID:  [16]byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0},
+			Valid: false,
+		}
 		return nil
 	}
+	u := n.UUID
 	err := u.Scan(v)
-	*n = NullUniqueIdentifier(u)
+	*n = NullUniqueIdentifier{
+		UUID:  u,
+		Valid: true,
+	}
 	return err
 }
 
 func (n NullUniqueIdentifier) Value() (driver.Value, error) {
-	return UniqueIdentifier(n).Value()
+	return n.UUID.Value()
 }
 
 func (n NullUniqueIdentifier) String() string {
-	return UniqueIdentifier(n).String()
+	return n.UUID.String()
 }
 
 func (n NullUniqueIdentifier) MarshalText() (text []byte, err error) {
-	return UniqueIdentifier(n).MarshalText()
+	return n.UUID.MarshalText()
 }
 
 func (n *NullUniqueIdentifier) UnmarshalJSON(b []byte) error {
-	u := UniqueIdentifier(*n)
+	u := n.UUID
 	err := u.UnmarshalJSON(b)
-	*n = NullUniqueIdentifier(u)
+	*n = NullUniqueIdentifier{
+		UUID:  u,
+		Valid: true,
+	}
 	return err
 }
