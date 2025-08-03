d0484de85862bf40749b7d3d3109159abd30ba2f
El-76 <maaaah@mail.ru>
2025-08-03 14:20:19 +0300

    Another bulk copy test.

--- bulkcopy_test.go
+++ bulkcopy_test.go
@@ -179,6 +179,7 @@ func testBulkcopy(t *testing.T, guidConversion bool) {
 		{"test_nullint32", sql.NullInt32{2147483647, true}, 2147483647},
 		{"test_nullint16", sql.NullInt16{32767, true}, 32767},
 		{"test_nulltime", sql.NullTime{time.Date(2010, 11, 12, 13, 14, 15, 120000000, time.UTC), true}, time.Date(2010, 11, 12, 13, 14, 15, 120000000, time.UTC)},
+		{"test_nulldecimal", decimal.NewNullDecimal(decimal.New(1232355, -4)), decimal.New(1232355, -4)},
 		{"test_datetimen_midnight", time.Date(2025, 1, 1, 23, 59, 59, 998_350_000, time.UTC), time.Date(2025, 1, 2, 0, 0, 0, 0, time.UTC)},
 		// {"test_smallmoney", 1234.56, nil},
 		// {"test_money", 1234.56, nil},
@@ -337,6 +338,8 @@ func compareValue(a interface{}, expected interface{}) bool {
 			return expected.Equal(got) && ez == az
 		}
 		return false
+	case decimal.Decimal:
+		return expected.Equal(a.(decimal.Decimal))
 	default:
 		return reflect.DeepEqual(expected, a)
 	}
@@ -442,6 +445,7 @@ func setupTable(ctx context.Context, t *testing.T, conn *sql.Conn, tableName str
 	[test_nullint32] [int] NULL,
 	[test_nullint16] [smallint] NULL,
 	[test_nulltime] [datetime] NULL,
+	[test_nulldecimal] [decimal](18, 4) NULL,
 	[test_datetimen_midnight] [datetime] NULL,
  CONSTRAINT [PK_` + tableName + `_id] PRIMARY KEY CLUSTERED
 (
