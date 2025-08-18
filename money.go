a1249b2735db985de8bcab4a70e576943fe5fae9
El-76 <maaaah@mail.ru>
2025-08-18 20:22:25 +0300

    Bug fix.

--- money.go
+++ money.go
@@ -1,5 +1,24 @@
 package mssql
 
-type Money[D any] struct {
+import (
+	"database/sql"
+	"database/sql/driver"
+
+	"github.com/shopspring/decimal"
+)
+
+type Money[D decimal.Decimal|decimal.NullDecimal] struct {
 	Decimal D
+}
+
+func (m Money[D]) Value() (driver.Value, error) {
+	valuer, _ := any(m.Decimal).(driver.Valuer)
+
+	return valuer.Value()
+}
+
+func (m *Money[D]) Scan(v any) error {
+	scanner, _ := any(&m.Decimal).(sql.Scanner)
+
+	return scanner.Scan(v);
 }
