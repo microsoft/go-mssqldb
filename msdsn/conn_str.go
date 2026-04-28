cc7df57c4d29381c2dfce3241196f66f513e9665
David Levy <dlevy@microsoft.com>
2026-04-28 17:18:28 -0500

    feat: add ADO.Net connection string synonyms
    Add 13 missing ADO.Net connection string synonyms to the adoSynonyms map:
    App, Connect Timeout, Timeout, Failover Partner, Failover Partner SPN,
    Application Intent, Trust Server Certificate, Multi Subnet Failover,
    Host Name In Certificate, Server SPN, Server Certificate, WSID.
    
    Includes 22 new test cases in TestValidConnectionString covering all
    new synonyms plus combined synonym usage.
    
    Closes #373
    Related to #288

--- msdsn/conn_str.go
+++ msdsn/conn_str.go
@@ -759,8 +759,9 @@ func (p Config) URL() *url.URL {
 	return &res
 }
 
-// ADO connection string keywords at https://github.com/dotnet/SqlClient/blob/main/src/Microsoft.Data.SqlClient/src/Microsoft/Data/Common/DbConnectionStringCommon.cs
+// ADO connection string keywords at https://learn.microsoft.com/en-us/dotnet/api/microsoft.data.sqlclient.sqlconnection.connectionstring
 var adoSynonyms = map[string]string{
+	"app":                       AppName,
 	"application name":          AppName,
 	"data source":               Server,
 	"address":                   Server,
@@ -770,6 +771,17 @@ var adoSynonyms = map[string]string{
 	"uid":                       UserID,
 	"pwd":                       Password,
 	"initial catalog":           Database,
+	"connect timeout":           ConnectionTimeout,
+	"timeout":                   ConnectionTimeout,
+	"failover partner":          FailoverPartner,
+	"failover partner spn":      FailoverPartnerSpn,
+	"application intent":        ApplicationIntent,
+	"trust server certificate":  TrustServerCertificate,
+	"multi subnet failover":     MultiSubnetFailover,
+	"host name in certificate":  HostNameInCertificate,
+	"server spn":                ServerSpn,
+	"server certificate":        ServerCertificate,
+	"wsid":                      WorkstationID,
 	"column encryption setting": "columnencryption",
 }
 
